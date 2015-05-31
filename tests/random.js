34d2fb37314d3eba6044126380abfa1c32d0d8fb
Jed Watson <jed.watson@me.com>
2015-05-31 11:25:36 +1000

    Fixed linter warnings in tests

--- tests/random.js
+++ tests/random.js
@@ -3,29 +3,29 @@
 var demand = require('must');
 var utils = require('../index');
 
-describe('Random strings', function() {
-	it('Should return a 10 char random string of alphanums', function() {
+describe('Random strings', function () {
+	it('Should return a 10 char random string of alphanums', function () {
 		demand(utils.randomString()).to.match(/^\w{10}$/i);
 	});
-	it('Should return a 2 char random string of alphanums', function() {
+	it('Should return a 2 char random string of alphanums', function () {
 		demand(utils.randomString(2)).to.match(/^\w{2}$/i);
 	});
-	it('Should return a 4-6 char random string of alphanums', function() {
-		demand(utils.randomString([4,6])).to.match(/^\w{4,6}$/i);
+	it('Should return a 4-6 char random string of alphanums', function () {
+		demand(utils.randomString([4, 6])).to.match(/^\w{4,6}$/i);
 	});
-	it('Should return a 4-6 char random string of alphanums', function() {
-		demand(utils.randomString([4,6])).to.match(/^\w{4,6}$/i);
+	it('Should return a 4-6 char random string of alphanums', function () {
+		demand(utils.randomString([4, 6])).to.match(/^\w{4,6}$/i);
 	});
-	it('Should return a 4-6 char random string of alphanums', function() {
-		demand(utils.randomString([4,6])).to.match(/^\w{4,6}$/i);
+	it('Should return a 4-6 char random string of alphanums', function () {
+		demand(utils.randomString([4, 6])).to.match(/^\w{4,6}$/i);
 	});
-	it('Should return a 4-6 char random string of alphanums', function() {
-		demand(utils.randomString([4,6])).to.match(/^\w{4,6}$/i);
+	it('Should return a 4-6 char random string of alphanums', function () {
+		demand(utils.randomString([4, 6])).to.match(/^\w{4,6}$/i);
 	});
-	it('Should return a 4-6 char random string of alphanums', function() {
-		demand(utils.randomString([4,6])).to.match(/^\w{4,6}$/i);
+	it('Should return a 4-6 char random string of alphanums', function () {
+		demand(utils.randomString([4, 6])).to.match(/^\w{4,6}$/i);
 	});
-	it('Should return a 20 char random string of "ABCDE"', function() {
+	it('Should return a 20 char random string of "ABCDE"', function () {
 		demand(utils.randomString(20, 'ABCDE')).to.match(/^[A-E]{20}$/);
 	});
 });
--- tests/strings.js
+++ tests/strings.js
@@ -3,153 +3,153 @@
 var demand = require('must');
 var utils = require('../index');
 
-describe('String transform', function(){
-	describe('Titlecase', function() {
-		it('Should transform "one" to "One" ', function() {
+describe('String transform', function () {
+	describe('Titlecase', function () {
+		it('Should transform "one" to "One" ', function () {
 			demand(utils.titlecase('one')).to.equal('One');
 		});
-		it('Should transform "one two" to "One Two"', function() {
+		it('Should transform "one two" to "One Two"', function () {
 			demand(utils.titlecase('one two')).to.equal('One Two');
 		});
-		it('Should transform "one Two three" to "One Two Three"', function() {
+		it('Should transform "one Two three" to "One Two Three"', function () {
 			demand(utils.titlecase('one Two three')).to.equal('One Two Three');
 		});
 	});
 
-	describe('Key to label', function() {
-		it('should transform "one" to "One"', function() {
+	describe('Key to label', function () {
+		it('should transform "one" to "One"', function () {
 			demand(utils.keyToLabel('one')).to.equal('One');
 		});
-		it('should transform "oneTwo" to "One Two" ', function() {
+		it('should transform "oneTwo" to "One Two" ', function () {
 			demand(utils.keyToLabel('oneTwo')).to.equal('One Two');
 		});
-		it('should transform "one_two" to "One_two"', function() {
+		it('should transform "one_two" to "One_two"', function () {
 			demand(utils.keyToLabel('one_two')).to.equal('One_two');
 		});
-		it('should transform "oneTwoThree" to "One Two Three"', function() {
+		it('should transform "oneTwoThree" to "One Two Three"', function () {
 			demand(utils.keyToLabel('oneTwoThree')).to.equal('One Two Three');
 		});
-		it('should transform "oneTWOThree" to "One TWOThree"', function() {
+		it('should transform "oneTWOThree" to "One TWOThree"', function () {
 			demand(utils.keyToLabel('oneTWOThree')).to.equal('One TWOThree');
 		});
-		it('should transform "one twoThree" to "One Two Three" ', function() {
+		it('should transform "one twoThree" to "One Two Three" ', function () {
 			demand(utils.keyToLabel('one twoThree')).to.equal('One Two Three');
 		});
-		it('should transform "oneTwo.three" to "One Two Three"', function() {
+		it('should transform "oneTwo.three" to "One Two Three"', function () {
 			demand(utils.keyToLabel('oneTwo.three')).to.equal('One Two Three');
 		});
-		it('should transform "oneTwo3four" to "One Two 3 Four"', function() {
+		it('should transform "oneTwo3four" to "One Two 3 Four"', function () {
 			demand(utils.keyToLabel('oneTwo3four')).to.equal('One Two 3 Four');
 		});
-		it('should transform "oneTwo-threeFour" to "One Two Three Four" ', function() {
+		it('should transform "oneTwo-threeFour" to "One Two Three Four" ', function () {
 			demand(utils.keyToLabel('oneTwo-threeFour')).to.equal('One Two Three Four');
 		});
-		it('should transform "id" to "Id"', function() {
+		it('should transform "id" to "Id"', function () {
 			demand(utils.keyToLabel('id')).to.equal('Id');
 		});
-		it('should transform "someId" to "Some Id"', function() {
+		it('should transform "someId" to "Some Id"', function () {
 			demand(utils.keyToLabel('someId')).to.equal('Some Id');
 		});
 	});
 
-	describe('Key to path', function() {
-		it('should transform "faq" to "faq"', function() {
+	describe('Key to path', function () {
+		it('should transform "faq" to "faq"', function () {
 			demand(utils.keyToPath('faq')).to.equal('faq');
 		});
-		it('should transform "FAQs" to "faqs"', function() {
+		it('should transform "FAQs" to "faqs"', function () {
 			demand(utils.keyToPath('FAQs')).to.equal('faqs');
 		});
-		it('should transform "theFAQs" to "the-faqs"', function() {
+		it('should transform "theFAQs" to "the-faqs"', function () {
 			demand(utils.keyToPath('theFAQs')).to.equal('the-faqs');
 		});
-		it('should transform "oneTwo" to "one-two"', function() {
+		it('should transform "oneTwo" to "one-two"', function () {
 			demand(utils.keyToPath('oneTwo')).to.equal('one-two');
 		});
-		it('should transform "One_two" to "one_two"', function() {
+		it('should transform "One_two" to "one_two"', function () {
 			demand(utils.keyToPath('One_two')).to.equal('one_two');
 		});
-		it('should transform "one2three" to "one-2-three"', function() {
+		it('should transform "one2three" to "one-2-three"', function () {
 			demand(utils.keyToPath('one2three')).to.equal('one-2-three');
 		});
-		it('should transform "id" to "id"', function() {
+		it('should transform "id" to "id"', function () {
 			demand(utils.keyToPath('id')).to.equal('id');
 		});
-		it('should transform "SomeId" to "some-id"', function() {
+		it('should transform "SomeId" to "some-id"', function () {
 			demand(utils.keyToPath('SomeId')).to.equal('some-id');
 		});
 	});
 
-	describe('Key to property', function() {
-		it('should transform "faq" to "faq"', function() {
+	describe('Key to property', function () {
+		it('should transform "faq" to "faq"', function () {
 			demand(utils.keyToProperty('faq')).to.equal('faq');
 		});
-		it('should transform "FAQs" to "faqs"', function() {
+		it('should transform "FAQs" to "faqs"', function () {
 			demand(utils.keyToProperty('FAQs')).to.equal('faqs');
 		});
-		it('should transform "theFAQs" to "theFaqs"', function() {
+		it('should transform "theFAQs" to "theFaqs"', function () {
 			demand(utils.keyToProperty('theFAQs')).to.equal('theFaqs');
 		});
-		it('should transform "oneTwo" to "oneTwo"', function() {
+		it('should transform "oneTwo" to "oneTwo"', function () {
 			demand(utils.keyToProperty('oneTwo')).to.equal('oneTwo');
 		});
-		it('should transform "One_two" to "one_two"', function() {
+		it('should transform "One_two" to "one_two"', function () {
 			demand(utils.keyToProperty('One_two')).to.equal('one_two');
 		});
-		it('should transform "one2three" to "one2Three"', function() {
+		it('should transform "one2three" to "one2Three"', function () {
 			demand(utils.keyToProperty('one2three')).to.equal('one2Three');
 		});
-		it('should transform "id" to "id"', function() {
+		it('should transform "id" to "id"', function () {
 			demand(utils.keyToProperty('id')).to.equal('id');
 		});
-		it('should return "someId" for "SomeId"', function() {
+		it('should return "someId" for "SomeId"', function () {
 			demand(utils.keyToProperty('SomeId')).to.equal('someId');
 		});
 	});
 
-	describe('Escape String', function() {
-		it('should properly escape the string The Koala\'s Sanctuary', function() {
+	describe('Escape String', function () {
+		it('should properly escape the string The Koala\'s Sanctuary', function () {
 			demand(utils.escapeString('The Koala\'s Sanctuary')).to.equal('The Koala\\\'s Sanctuary');
 		});
-		it('should properly escape the string "Koala"', function() {
+		it('should properly escape the string "Koala"', function () {
 			demand(utils.escapeString('"Koala"')).to.equal('\\"Koala\\"');
 		});
-		it('should properly escape the string "This \\ That"', function() {
+		it('should properly escape the string "This \\ That"', function () {
 			demand(utils.escapeString('This \\ That')).to.equal('This \\\\ That');
 		});
 	});
 
-	describe('Diacritics', function() {
-		it('should transform "ÁÂÃÅÇÈÉàáâãåèéêëìíîïòóôõ" to "aaaaceeaaaaaeeeeiiiioooo"', function() {
+	describe('Diacritics', function () {
+		it('should transform "ÁÂÃÅÇÈÉàáâãåèéêëìíîïòóôõ" to "aaaaceeaaaaaeeeeiiiioooo"', function () {
 			demand(utils.slug('ÁÂÃÅÇÈÉàáâãåèéêëìíîïòóôõ')).to.equal('aaaaceeaaaaaeeeeiiiioooo');
 		});
-		it('should transform "Ää" to "aeae"', function() {
+		it('should transform "Ää" to "aeae"', function () {
 			demand(utils.slug('Ää')).to.equal('aeae');
 		});
-		it('should transform "çë Díaz Ü" to "ce-diaz-ue"', function() {
+		it('should transform "çë Díaz Ü" to "ce-diaz-ue"', function () {
 			demand(utils.slug('çë Díaz Ü')).to.equal('ce-diaz-ue');
 		});
 	});
 
-	describe('Transliteration', function() {
-		it('should transform "современном" to "sovremennom"', function() {
+	describe('Transliteration', function () {
+		it('should transform "современном" to "sovremennom"', function () {
 			demand(utils.slug('современном')).to.equal('sovremennom');
 		});
-		it('should transform "Балы, красавицы, лакеи, юнкера" to "baly-krasavicy-lakei-yunkera"', function() {
+		it('should transform "Балы, красавицы, лакеи, юнкера" to "baly-krasavicy-lakei-yunkera"', function () {
 			demand(utils.slug('Балы, красавицы, лакеи, юнкера')).to.equal('baly-krasavicy-lakei-yunkera');
 		});
-		it('should transform "И вальсы Шуберта и хруст французской булки," to "i-valsy-shuberta-i-hrust-francuzskoj-bulki"', function() {
+		it('should transform "И вальсы Шуберта и хруст французской булки," to "i-valsy-shuberta-i-hrust-francuzskoj-bulki"', function () {
 			demand(utils.slug('И вальсы Шуберта и хруст французской булки,')).to.equal('i-valsy-shuberta-i-hrust-francuzskoj-bulki');
 		});
-		it('should transform "Любовь, шампанское, закаты, переулки" to "lyubov-shampanskoe-zakaty-pereulki"', function() {
+		it('should transform "Любовь, шампанское, закаты, переулки" to "lyubov-shampanskoe-zakaty-pereulki"', function () {
 			demand(utils.slug('Любовь, шампанское, закаты, переулки')).to.equal('lyubov-shampanskoe-zakaty-pereulki');
 		});
-		it('should transform "Как упоительны в России вечера!" to "kak-upoitelny-v-rossii-vechera"', function() {
+		it('should transform "Как упоительны в России вечера!" to "kak-upoitelny-v-rossii-vechera"', function () {
 			demand(utils.slug('Как упоительны в России вечера!')).to.equal('kak-upoitelny-v-rossii-vechera');
 		});
 	});
 
-	describe('Smart apostrophe', function() {
-		it('should transform "The User’s Guide" to "the-users-guide"', function() {
+	describe('Smart apostrophe', function () {
+		it('should transform "The User’s Guide" to "the-users-guide"', function () {
 			demand(utils.slug('The User’s Guide')).to.equal('the-users-guide');
 		});
 	});
